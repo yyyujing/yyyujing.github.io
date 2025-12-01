09caf7dcf87024a53cd9da5e80775197b7076b54
Jing <jing.yu7@mail.mcgill.ca>
2025-11-30 23:45:39 -0800

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
